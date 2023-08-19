d177e0b5095d3df66fdae7641e5e6873488bc3cc
XQLYM5 <97853713+XQLYM5@users.noreply.github.com>
2023-08-19 16:34:03 -0400

    Add registratoin form Validate username Fix bugs with loading database

--- src/ludusultimatetictactoe/UserDatabase.java
+++ src/ludusultimatetictactoe/UserDatabase.java
@@ -14,13 +14,18 @@ value6,7,8,9,10 ...
 username,encryptedPassword,rating,wins,losses,currentGame,gamesPlayed,dateJoined
 */
 
-
+// need to user .loadDatabase(); before adding user
 public class UserDatabase {
     private String USER_DATABASE_FILE_PATH = "userDatabase.txt";
     private String userDatabaseEncryptionKey = config.userDataBaseEncryptionKey;
-    private ArrayList<User> userDatabase = new ArrayList<>();
 
-    public ArrayList<User> loadDatabase() {
+    public ArrayList<User> getUserDatabase() {
+        return userDatabase;
+    }
+    private ArrayList<User> userDatabase = new ArrayList<>();
+    
+    // loads the database from the file, returns done if done
+    public String loadDatabase() {
         String fileData = "";
         try{
             BufferedReader reader = new BufferedReader(new FileReader(USER_DATABASE_FILE_PATH));
@@ -36,18 +41,50 @@ public class UserDatabase {
             e.printStackTrace();
         }
         
+        // remove all newlines from end of file
+        fileData.replaceAll("[\n\r]$", "");
+        // check if empty string or .trim() is empty
+        if(fileData == null || fileData.trim().isEmpty()) {
+            return "done";
+        }
+        
         // decrypted database
         String decrypted = decryptDataBase(fileData);
         
         // spilt the decrypted database into an array of user strings
         String [] users = decrypted.split(System.lineSeparator());
-        
         for (String userString: users) {
+            System.out.println(userString);
             User currentUser = new User(userString);
             userDatabase.add(currentUser);
         }
         
-        return userDatabase;
+        return "done";
+    }
+    
+    public User getUser(String userName, String password) {
+        // perform linear search on user database.
+        // todo hash maps instead
+        for (User user: userDatabase) {
+            if (user.getUsername().equals(userName)) {
+                String inputEncryptedPassword = SHAEncryption.getSHA256Hash(password);
+                // check if the encrypted input password is equal to the actual user password
+                if (user.getEncryptedPassword().equals(inputEncryptedPassword)) {
+                    return user;
+                }
+            }
+        }
+        // otherwise return null.
+        return null;
+    }
+    
+    // also saves the user DB
+    public User addUser(String userName, String password) {
+        User newUser = new User(userName, password);
+        userDatabase.add(newUser);
+        saveUserDatabase(userDatabase);
+        return newUser;
+        
     }
 
     public UserDatabase() {
@@ -65,6 +102,7 @@ public class UserDatabase {
         String encryptedFile = encryptDataBase(fileData);
         
         try {
+            System.out.println("Saving file");
             BufferedWriter writer = new BufferedWriter(new FileWriter(USER_DATABASE_FILE_PATH));
             writer.write(encryptedFile);
             writer.close();
